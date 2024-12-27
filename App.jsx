bf6e8c4509c45e8d6d0b5aeb59fab76bfff1fd35
Ejaz khan <79781448+ejazkhan22@users.noreply.github.com>
2024-12-27 20:05:28 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Login from './compunents/Login/Login'
+import SignUp from './compunents/Sign-UP/SignUp'
+function App() {
+
+
+  return (
+    <>
+    <div className="main">
+
+    <div className="container">
+
+   <SignUp></SignUp>
+   {/* <Login></Login> */}
+     </div>
+    </div>
+
+    </>
+  )
+}
+
+export default App
